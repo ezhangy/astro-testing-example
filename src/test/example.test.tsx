72ac9863d2e01044778490d6f65382613a7ca33d
elizabeth zhang <elizabeth.zhangey@gmail.com>
2024-07-02 09:59:03 -0400

    add example with Counter component

---
+++ src/test/example.test.tsx
@@ -0,0 +1,16 @@
+import { loadRenderers } from "astro:container";
+import { expect, test } from "vitest";
+import { getContainerRenderer as reactContainerRenderer } from "@astrojs/react";
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import IndexPage from "../pages/index.astro";
+
+test("Counter component contains the word 'count'", async () => {
+    const renderers = await loadRenderers([reactContainerRenderer()]);
+    const container = await AstroContainer.create({
+        renderers,
+    });
+    const result = await container.renderToString(IndexPage, {});
+    console.log({ result })
+    expect(result).toContain("Count")
+
+})
