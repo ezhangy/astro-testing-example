a6fdb8a1c055835daaf0ec7975807867eae9ae1f
elizabeth zhang <elizabeth.zhangey@gmail.com>
2024-07-02 10:01:25 -0400

    add ReactWrapper component

--- src/test/example.test.tsx
+++ src/test/example.test.tsx
@@ -3,13 +3,14 @@ import { expect, test } from "vitest";
 import { getContainerRenderer as reactContainerRenderer } from "@astrojs/react";
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import IndexPage from "../pages/index.astro";
+import ReactWrapper from "../components/ReactWrapper.astro";
 
 test("Counter component contains the word 'count'", async () => {
     const renderers = await loadRenderers([reactContainerRenderer()]);
     const container = await AstroContainer.create({
         renderers,
     });
-    const result = await container.renderToString(IndexPage, {});
+    const result = await container.renderToString(ReactWrapper);
     console.log({ result })
     expect(result).toContain("Count")
 
